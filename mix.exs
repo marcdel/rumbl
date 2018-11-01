e0d243be8a45cac7e88a3cbdb45bb80a056fb1e0
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-11-01 16:43:25 -0700

    Adds user credentials with hashed passwords

--- mix.exs
+++ mix.exs
@@ -43,7 +43,10 @@ defmodule Rumbl.MixProject do
       {:phoenix_live_reload, "~> 1.2-rc", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:comeonin, "~> 4.1"},
+      {:bcrypt_elixir, "~> 1.0"},
+      {:pbkdf2_elixir, "~> 0.12"}
     ]
   end
 
