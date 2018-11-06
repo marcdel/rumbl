300275ea7f83cc583f395b87a2c90c8681bcaf73
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-11-06 10:13:30 -0800

    Adds ability for users to manage videos at /manage/videos

---
+++ lib/rumbl/multimedia/multimedia.ex
@@ -0,0 +1,67 @@
+defmodule Rumbl.Multimedia do
+  import Ecto.Query, warn: false
+  alias Rumbl.Accounts
+  alias Rumbl.Multimedia.Video
+  alias Rumbl.Repo
+
+  def list_user_videos(%Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
+    |> preload_user()
+  end
+
+  def list_videos do
+    Video
+    |> Repo.all()
+    |> preload_user()
+  end
+
+  def get_user_video!(%Accounts.User{} = user, id) do
+    from(v in Video, where: v.id == ^id)
+    |> user_videos_query(user)
+    |> Repo.one!()
+    |> preload_user()
+  end
+
+  def get_video!(id) do
+    Video
+    |> Repo.get!(id)
+    |> preload_user()
+  end
+
+  def create_video(%Accounts.User{} = user, attrs \\ %{}) do
+    %Video{}
+    |> Video.changeset(attrs)
+    |> put_user(user)
+    |> Repo.insert()
+  end
+
+  def update_video(%Video{} = video, attrs) do
+    video
+    |> Video.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_video(%Video{} = video) do
+    Repo.delete(video)
+  end
+
+  def change_video(%Accounts.User{} = user, %Video{} = video) do
+    video
+    |> Video.changeset(%{})
+    |> put_user(user)
+  end
+
+  defp put_user(changeset, user) do
+    Ecto.Changeset.put_assoc(changeset, :user, user)
+  end
+
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
+  end
+
+  defp preload_user(video_or_videos) do
+    Repo.preload(video_or_videos, :user)
+  end
+end
