c1acf4c9b5358fa270f716c9b30a5af685880e40
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-11-22 15:24:04 -0800

    Returns only results that are received within the set timeout

--- lib/rumbl/info_sys/info_sys.ex
+++ lib/rumbl/info_sys/info_sys.ex
@@ -2,17 +2,29 @@ defmodule Rumbl.InfoSys do
   alias Rumbl.InfoSys
 
   @backends [InfoSys.Wolfram]
+  @timeout 10_000
 
   defmodule Result do
     defstruct score: 0, text: nil, url: nil, backend: nil
   end
 
   def compute(query, opts \\ []) do
-    opts = Keyword.put_new(opts, :limit, 10)
+    timeout = opts[:timeout] || @timeout
     backends = opts[:backends] || @backends
+    opts = Keyword.put_new(opts, :limit, 10)
 
     backends
     |> Enum.map(&async_query(&1, query, opts))
+    |> Task.yield_many(timeout)
+    |> Enum.map(fn {task, response} ->
+      response || Task.shutdown(task, :brutal_kill)
+    end)
+    |> Enum.flat_map(fn
+      {:ok, results} -> results
+      {:error, _reason} -> []
+    end)
+    |> Enum.sort(&(&1.score >= &2.score))
+    |> Enum.take(opts[:limit])
   end
 
   defp async_query(backend, query, opts) do
